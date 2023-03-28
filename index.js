8bd50abba63bc0d6079d3e6d97f10beca5538dab
Ankitstr <124289461+Ankitstr@users.noreply.github.com>
2023-03-28 16:02:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -48,11 +48,5 @@ app.listen(process.env.PORT, (error) => {
    if (error) {
       console.log("error in the port");
    }
-   console.log(
-      `     
-
-      ******************* HEY! SERVER IS RUNNING on Link :  http://localhost:${process.env.PORT} **********************
-      
-      `
-   );
+   console.log(` ******************* HEY! SERVER IS RUNNING  **********************  `  );
 });
